77d06d3018933035a523e663721ce15e8d49514b
Axel W <axelwestman@outlook.com>
2024-07-25 15:47:13 -0300

    agregado de foto de los pokemones

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from 'react'
+import './index.css';
 
 function App() {
 
-  let [pokemonNumber, setPokemonNumber] = useState(0);
+  let [pokemonNumber, setPokemonNumber] = useState(1);
   let [pokemonName, setPokemonName] = useState('');
+  let [pokemonPhoto, setPokemonPhoto] = useState('');
 
   function incrementarNumero(){
     setPokemonNumber(pokemonNumber  + 1);
+  }
+
+  function restarNumero(){
+    setPokemonNumber(pokemonNumber  - 1);
   }
 
  useEffect(() =>{
@@ -23,12 +29,21 @@ function App() {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`)
   const data = await response.json()
   setPokemonName(data.name)
+  setPokemonPhoto(data.sprites.front_default)
  }
 
   return (
     <>
-      <button onClick={incrementarNumero}>Next</button>
-      <div>{pokemonNumber} - {pokemonName}</div>
+    <div className="w-full h-screen bg-violet-500 flex items-center justify-center">
+      <div className="border h-72 w-72 text-center flex flex-col justify-center">
+        <div className="flex items-center justify-center"><img src={pokemonPhoto} alt="pokemon-foto"  className='w-52' /></div>
+        <div>{pokemonNumber} - {pokemonName}</div>
+          <div className="flex justify-center gap-10">
+           <button onClick={restarNumero} className={pokemonNumber == 0 ? 'hidden' : 'bg-red-500 w-16 h-auto'}>Back</button>
+            <button button onClick={incrementarNumero} className="bg-red-500 w-16 h-auto">Next</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
